ecfea54ad181e4ae96b03b5b12c1c2dd2077fad4
Fernando Paredes <nanoid.xd@gmail.com>
2013-04-30 20:24:01 -0700

    Add navigational controls and tests

--- spec/features/navigate_movies_spec.rb
+++ spec/features/navigate_movies_spec.rb
@@ -2,15 +2,19 @@ require "spec_helper"
 
 describe "Navigating movies" do
   it "allows navigation from the detail page to the listing page" do
-    movie = Movie.create(title: "Iron Man",
-                         rating: "PG-13",
-                         total_gross: 318412101.00,
-                         description: "Tony Stark builds an armored suit to fight the throes of evil",
-                         released_on: "2008-05-02")
+    movie = Movie.create(movie_attributes)
 
     visit movie_url(movie)
     click_link "All Movies"
 
     expect(current_path).to eq(movies_path)
+  end
+
+  it "allows navigating from the listing page to the detail page" do
+    movie = Movie.create(movie_attributes)
+    visit movies_url
+    click_link movie.title
+
+    expect(current_path).to eq(movie_path(movie))
   end
 end
