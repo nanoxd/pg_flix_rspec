762367ba1148d25540a594321bcf8f2f7005482f
Fernando Paredes <nanoid.xd@gmail.com>
2013-05-15 09:00:33 -0700

    Add images, director, cast, and runtime information to show page

--- spec/features/create_movie_spec.rb
+++ spec/features/create_movie_spec.rb
@@ -12,6 +12,10 @@ describe "Creating a new movie" do
     fill_in "Description", with: "Superheroes saving the world from villains"
     fill_in "Rating", with: "PG-13"
     fill_in "Total gross", with: "75000000"
+    fill_in "Cast", with: "The award-winning cast"
+    fill_in "Director", with: "The ever-creative director"
+    fill_in "Duration", with: "123 min"
+    fill_in "Image file name", with: "movie.png"
     select (Time.now.year - 1).to_s, :from => "movie_released_on_1i"
 
     click_button 'Create Movie'
@@ -20,5 +24,7 @@ describe "Creating a new movie" do
 
     expect(page).to have_text('New Movie Title')
   end
+
+
 
 end
